3cb87e7dd845cb27fcc00fcab135a40b150a6b07
hiren polara <hiren.polara22@gmail.com>
2022-05-07 00:09:02 +0530

    faqs ui page setup

--- src/js/main.js
+++ src/js/main.js
@@ -88,3 +88,35 @@ $(".mega-menu-back").click(function () {
     $("body").removeClass("fixed-bg");
     $(".mega-menu").removeClass("open");
 });
+
+
+// tab filter js
+
+$(document).ready(function(){
+    $(".filter-tab-btn").click(function(){
+        var value = $(this).attr('data-filter');
+
+        if(value == "all")
+        {
+            $('.filter').show('700');
+        }
+        else
+        {
+            $(".filter").not('.'+value).hide('2000');
+            $('.filter').filter('.'+value).show('2000');
+        }
+
+        var filterOpen = $(this).hasClass("active");
+
+        if(filterOpen){
+            $('.filter-tab-btn').removeClass('active');
+        }else {
+            var siblings = $('.filter-tab-nav').find(".active");
+            siblings.removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+
+});
+
+// END tab filter js
